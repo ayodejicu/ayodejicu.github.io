f4bc192774e363c7feab282df413bc99622f524d
ayodejicu <119212430+ayodejicu@users.noreply.github.com>
2022-11-27 16:48:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,209 @@
+function cickeddeji1(){
+    if(deji1==1){
+        document.getElementById("deji1").style.visibility="hidden"
+        return deji1=0;
+    }
+
+    else{
+        document.getElementById("deji1").style.visibility="visible"
+        return deji1=1;
+    }
+}
+
+
+
+
+
+function clickeddaddy1(){
+    if(daddy1==1){
+        document.getElementById("daddy1").style.visibility="hidden"
+        return daddy1=0;
+    }
+
+    else{
+        document.getElementById("daddy1").style.visibility="visible"
+        return daddy1=1;
+    }
+}
+
+
+
+function clickeddunni1(){
+    if(dunni1==1){
+        document.getElementById("dunni1").style.visibility="hidden"
+        return dunni1=0;
+    }
+
+    else{
+        document.getElementById("dunni1").style.visibility="visible"
+        return dunni1=1;
+    }
+}
+
+
+
+function clickedmummy1(){
+    if(mummy1==1){
+        document.getElementById("mummy1").style.visibility="hidden"
+        return mummy1=0;
+    }
+
+    else{
+        document.getElementById("mummy1").style.visibility="visible"
+        return mummy1=1;
+    }
+}
+
+
+function clicked(){
+    if(ifedami1==1){
+        document.getElementById("ifedami1").style.visibility="hidden"
+        return ifedami1=0;
+    
+    }
+
+    else{
+        document.getElementById("ifedami1").style.visibility="visible"
+        return ifedami1=1;
+    }
+}
+
+
+var displayedTexts = document.getElementsByClassName("displayText");
+
+function arr_0(){
+displayedTexts[0].style.visibility = "visible";
+displayedTexts[1].style.display = "inline";
+displayedTexts[2].style.display = "none";
+displayedTexts[3].style.display = "none";
+displayedTexts[4].style.display = "none";
+displayedTexts[5].style.display = "none";
+
+}
+
+
+function arr_1(){
+    displayedTexts[0].style.visibility = "visible";
+    displayedTexts[1].style.display = "none";
+    displayedTexts[2].style.display = "inline";
+    displayedTexts[3].style.display = "none";
+    displayedTexts[4].style.display = "none";
+    displayedTexts[5].style.display = "none";
+    
+}
+
+function arr_2(){
+    displayedTexts[0].style.visibility = "visible";
+    displayedTexts[1].style.display = "none";
+    displayedTexts[2].style.display = "none";
+    displayedTexts[3].style.display = "inline";
+    displayedTexts[4].style.display = "none";
+    displayedTexts[5].style.display = "none";
+    
+}
+
+function arr_3(){
+    displayedTexts[0].style.visibility = "visible";
+    displayedTexts[1].style.display = "none";
+    displayedTexts[2].style.display = "none";
+    displayedTexts[3].style.display = "none";
+    displayedTexts[4].style.display = "inline";
+    displayedTexts[5].style.display = "none";
+    
+}
+
+function arr_4(){
+    displayedTexts[0].style.visibility = "visible";
+    displayedTexts[1].style.display = "none";
+    displayedTexts[2].style.display = "none";
+    displayedTexts[3].style.display = "none";
+    displayedTexts[4].style.display = "none";
+    displayedTexts[5].style.display = "inline";
+    
+}
+
+
+
+
+var dbt_s = document.getElementsByClassName("written");
+
+function dbt_0(){
+    dbt_s[0].style.visibility = "visible";
+    dbt_s[1].style.visibility = "hidden";
+    dbt_s[2].style.visibility = "hidden";
+    dbt_s[3].style.visibility = "hidden";
+    dbt_s[4].style.visibility = "hidden";
+}
+
+function dbt_1(){
+    dbt_s[0].style.visibility = "hidden";
+    dbt_s[1].style.visibility = "visible";
+    dbt_s[2].style.visibility = "hidden";
+    dbt_s[3].style.visibility = "hidden";
+    dbt_s[4].style.visibility = "hidden";
+}
+
+function dbt_2(){
+    dbt_s[0].style.visibility = "hidden";
+    dbt_s[1].style.visibility = "hidden";
+    dbt_s[2].style.visibility = "visible";
+    dbt_s[3].style.visibility = "hidden";
+    dbt_s[4].style.visibility = "hidden";
+}
+
+function dbt_3(){
+    dbt_s[0].style.visibility = "hidden";
+    dbt_s[1].style.visibility = "hidden";
+    dbt_s[2].style.visibility = "hidden";
+    dbt_s[3].style.visibility = "visible";
+    dbt_s[4].style.visibility = "hidden";
+}
+
+function dbt_4(){
+    dbt_s[0].style.visibility = "hidden";
+    dbt_s[1].style.visibility = "hidden";
+    dbt_s[2].style.visibility = "hidden";
+    dbt_s[3].style.visibility = "hidden";
+    dbt_s[4].style.visibility = "visible";
+}
+
+
+
+
+
+
+
+
+
+
+
+var Hidingp = document.getElementsByClassName("hidem");
+
+function clik4(){
+    Hidingp[0].style.visibility = "hidden";
+    Hidingp[1].style.visibility = "hidden";
+    Hidingp[2].style.visibility = "hidden";
+    Hidingp[3].style.visibility = "hidden";
+    Hidingp[4].style.visibility = "hidden";
+    Hidingp[5].style.visibility = "hidden";
+    Hidingp[6].style.visibility = "hidden";
+    Hidingp[7].style.visibility = "hidden";
+    Hidingp[8].style.visibility = "hidden";
+    Hidingp[9].style.visibility = "hidden";
+    Hidingp[10].style.visibility = "hidden";
+    Hidingp[11].style.visibility = "hidden";
+    Hidingp[12].style.visibility = "hidden";
+    Hidingp[13].style.visibility = "hidden";
+    Hidingp[14].style.visibility = "hidden";
+    Hidingp[15].style.visibility = "hidden";
+    Hidingp[16].style.visibility = "hidden";
+}
+
+function click1()   {
+            arr_0();
+            cickeddeji1();
+        }
+
+
+
+
